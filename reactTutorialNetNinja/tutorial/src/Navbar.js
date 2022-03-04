d6a9d8e336c850c9a8d8c898bd758965260413e2
AnastasiaJakowenko <jakowenko.anastasia1313@gmail.com>
2022-03-04 14:56:16 +0100

    jj

---
+++ reactTutorialNetNinja/tutorial/src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {    //sfc tab um zu create const und sofort default
+    return ( 
+        <nav className="navbar">
+            <h1>The Nastjas Blog</h1>
+            <div className="links">
+            <a href="/">Home</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
